469c9e2f9e5086be2ace7e8ec7946e0108872322
Anonymint <mint.com@gmail.com>
2017-08-03 17:19:46 -0500

    Update script to start and stop
    I have crazy idea to test it
    
    Add jdk8 to infrastructure
    
    Update script to start
    
    Upgrade jdk to 8 instead of 7
    
    Update infrastructure file
    
    Add script to start
    
    Update infra script
    
    Update startApplication script
    
    Update startApplication script
    
    Update startApplication script
    
    Add sleep to validate, wait for 5 sec before validate
    
    Add sleep to validate, wait for 10 sec before validate

--- codedeploy/ApplicationStop.sh
+++ codedeploy/ApplicationStop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kill $(ps aux | grep '[s]impleapi' | awk '{print $2}')
+kill $(ps aux | grep '[s]impleapi' | awk '{print $2}') > /dev/null 2>&1
 exit 0
--- codedeploy/ValidateService.sh
+++ codedeploy/ValidateService.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+ps aux | grep java
+sleep 10
+curl http://127.0.0.1:8000
 curl http://localhost:8000
