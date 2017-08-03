edf9a8e2b2c2297042572972d267aa6f05bf50e1
Anonymint <mint.com@gmail.com>
2017-08-03 17:03:53 -0500

    Update validate script

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /opt/simpleapi-0.0.1-SNAPSHOT.jar & echo $! > /dev/null 2>&1 & echo $! > /opt/pid.file
+java -jar /opt/simpleapi-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 & echo $! > /opt/pid.file
